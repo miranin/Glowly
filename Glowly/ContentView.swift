2655fa418a77e83d2190de7ab381a60f77e83605
tamerlan.aubaqirov <tamerlan.aubaqirov@octogroup.uz>
2025-10-02 10:58:32 +0500

    Initial Commit

---
+++ Glowly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glowly
+//
+//  Created by Tamirlan Aubakirov on 02/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glowly/GlowlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowlyApp.swift
+//  Glowly
+//
+//  Created by Tamirlan Aubakirov on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GlowlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
